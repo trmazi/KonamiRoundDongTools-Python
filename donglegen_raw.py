9a329d96c4fff85d85e4a5d71d5301b4e3b4ee18
anon <anon>
2022-01-04 22:19:08 -0500

    donglegen_raw.py: review changes

--- donglegen_raw.py
+++ donglegen_raw.py
@@ -1,22 +1,19 @@
 import sys
 
-from tools.structs import staticValues, dataStructs
 from tools.compileinfo import CompileDong
-from tools.mcodetools import mcodeTools
-from tools.systemtext import generateSystemPrints
-from tools.filetools import fileTools
 
 if len(sys.argv) != 4:
     print(f"donglegen_raw <key> <mcode> <pcbid>")
     print("")
     exit()
 
-in_key = sys.argv[1]
-in_mcode = sys.argv[2]
-in_pcbid = sys.argv[3]
+in_key = sys.argv[1].upper()
+in_mcode = sys.argv[2].upper()
+in_pcbid = sys.argv[3].upper()
 
 if len(in_key) != 8:
     print(f"invalid key.")
+    exit()
 
 if len(in_mcode) != 8 or in_mcode[0] != 'G':
     print("invalid mcode.")
@@ -24,6 +21,7 @@ if len(in_mcode) != 8 or in_mcode[0] != 'G':
 
 if len(in_pcbid) != 8:
     print(f"invalid pcbid.")
+    exit()
 
 compileddong = CompileDong.makeBlackDong(in_key, in_mcode, in_pcbid)
 open(f"raw_{in_mcode}_{in_pcbid}", "wb").write(bytes(compileddong))
